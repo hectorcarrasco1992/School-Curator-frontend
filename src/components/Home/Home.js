4b5dc9406ffa310a97328ec6666e6213d33a01d0
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2020-08-25 01:56:13 -0400

    changed wording on landing page and removed you from nav

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,29 +21,29 @@ export default class Home extends Component {
                     
                     <section>
                         <div className="parallax-one">
-                            <h2>Believe you can and you're halfway there. <br/>-Theodore Roosevelt</h2>
+                            <h2>'Believe you can and you're halfway there.' <br/>-Theodore Roosevelt</h2>
                         </div>
                     </section>
 
                     <section>
                         <div className="block">
-                            <p><span className="first-character sc">I</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae aspernatur non minus, error nisi laudantium cumque blanditiis similique sapiente. Doloribus enim quae dicta aliquam libero et consectetur placeat omnis blanditiis!</p>
+                            <p><span className="first-character sc">I</span>can do this! This mentality is the first step into learning a new skill and starting a new career.</p>
                             <p className="line-break margin-top-10"></p>
-                            <p className="margin-top-10">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam in nobis fugit voluptatibus at, alias vero libero esse inventore numquam eligendi perferendis asperiores tenetur debitis. Labore voluptate iure doloribus? Labore.</p>
+                            <p className="margin-top-10">Self belief is the vehicle that will carry you throughout your life and having it is an essential tool. Only thing that can stop you is you! Find your Dojo here!!</p>
                         </div>
                     </section>
                     
                     <section>
                         <div className="parallax-two">
-                            <h2>Learn the Skills</h2>
+                            <h2>'Any fool can know. The point is to understand.' –Albert Einstein</h2>
                         </div>
                     </section>
                     
                     <section>
                         <div className="block">
-                            <p><span className="first-character ny">B</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, saepe. Odit maiores animi impedit reiciendis libero qui exercitationem illum nulla magnam, praesentium modi perferendis deserunt voluptas vitae veniam, pariatur perspiciatis.</p>
+                            <p><span className="first-character ny">B</span>ecome a better you and find a school that will help you understand code, not just learn it. Anyone can be taught what to write but a good school can help you learn to think like a coder. With this new mindset, you can become the next Marc Zuckleburg without the drama.</p>
                             <p className="line-break margin-top-10"></p>
-                            <p className="margin-top-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ab recusandae commodi ut minima excepturi quaerat debitis delectus eos praesentium alias animi, deserunt sequi quo! Incidunt aliquid molestiae corrupti unde.</p>
+                            <p className="margin-top-10"> ‘Tell me and I forget. Teach me and I remember. Involve me and I learn.’ –Benjamin Franklin</p>
                         </div>
                     </section>
                     
@@ -55,9 +55,9 @@ export default class Home extends Component {
                     
                     <section>
                         <div className="block">
-                            <p><span className="first-character atw">W</span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque necessitatibus ea similique laboriosam enim? Similique ipsa iusto, aliquid soluta expedita facere ab assumenda dicta est dolores libero consequatur adipisci nostrum!</p>
+                            <p><span className="first-character atw">F</span>ind your team in the classroom. Get surrounded by amazing teachers and classmates who will further propel your journey in coding.</p>
                             <p className="line-break margin-top-10"></p>
-                            <p className="margin-top-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam quis quas optio, maiores atque aliquam. Quos numquam necessitatibus, dicta similique inventore esse explicabo doloribus nam. Omnis architecto earum dolorum repellendus.</p>
+                            <p className="margin-top-10">'Teamwork is the ability to work together toward a common vision. The ability to direct individual accomplishments toward organizational objectives. It is the fuel that allows common people to attain uncommon results.' -Andrew Carnegie</p>
                         </div>
                     </section>
                 </div>
